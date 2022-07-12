e920651ae587bc70952aefedb1b93b82394e0ed5
e601201 <k.arthur1111@gmail.com>
2022-07-12 21:10:50 +0900

    [add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,42 @@
 Rails.application.routes.draw do
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development? #letter_openerの利用inDev環境
   root 'static_pages#welcome'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do
+    root 'dashboards#index'
+    get  'login',       to: 'user_sessions#new'                     #管理者ログイン画面
+    post 'login',       to: 'user_sessions#create'                  #管理者ログイン処理
+    delete 'logout',    to: 'user_sessions#destroy'                 #管理者ログアウト処理
+    resources :projects, except: %i[new create]                     #projects管理
+    resources :workspaces, except: %i[new create]                   #workspaces管理
+    resources :tasks, except: %i[new create]                        #tasks管理
+  end
+
+  get  'login',       to: 'user_sessions#new'                       #ユーザーログイン画面
+  post 'login',       to: 'user_sessions#create'                    #ユーザーログイン処理
+  delete 'logout',    to: 'user_sessions#destroy'                   #ユーザーログアウト処理
+  resources :password_resets, only: %i[new create edit update]      #パスワードリセット
+
+  resources :projects do                                            #projects
+    resources :tasks                                                #tasks
+    resource :preview, only: :show, module: 'projects'              #task設定時のprojectのプレビュー表示
+    resources :favorites, only: %i[create destroy]                  #projectをお気に入りに追加
+    collection do
+      get 'search'                                                  #projectsの検索
+      resource :recent, only: :show, module: 'projects'             #projectの新規作成順
+      resource :hot, only: :show, module: 'projects'                #お気に入りの多いproject順
+    end
+  end
+
+  resources :workspaces do                                          #workspaces_CRUD
+    resource :profile, only: %i[edit update]
+    resource :relationships, only: %i[create destroy]               #好きなwaorkspaceをお気に入りに追加
+    member do
+      resource :tool_set, except: [:new]                            #任意のworkspaceの道具一覧(workspaceのnew時に一緒にnewする)
+      get 'projects'                                                #プロジェクト一覧
+      get 'followers'                                               #フォロワー一覧
+      get 'followings'                                              #フォロー一覧
+      get 'favorites'                                               #お気に入り一覧
+    end
+  end
 end
