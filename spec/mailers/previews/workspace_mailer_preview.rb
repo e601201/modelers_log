837c929b01b67b35b43c867ac896161646dfcefe
DaichiNagata <90130769+e601201@users.noreply.github.com>
2022-07-14 23:37:26 +0900

    Revert "Remember me system"

--- spec/mailers/previews/workspace_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/workspace_mailer
-class WorkspaceMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/workspace_mailer/reset_password_email
-  def reset_password_email
-    WorkspaceMailer.reset_password_email
-  end
-
-end
