412b318b236218e06f3a01049c05f7aace6b4a7e
e601201 <k.arthur1111@gmail.com>
2022-07-21 15:19:51 +0900

    "[MOD]layouts"

--- config/routes.rb
+++ config/routes.rb
@@ -4,39 +4,40 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'dashboards#index'
-    get  'login',       to: 'user_sessions#new'                     #管理者ログイン画面
-    post 'login',       to: 'user_sessions#create'                  #管理者ログイン処理
-    delete 'logout',    to: 'user_sessions#destroy'                 #管理者ログアウト処理
-    resources :projects, except: %i[new create]                     #projects管理
-    resources :workspaces, except: %i[new create]                   #workspaces管理
-    resources :tasks, except: %i[new create]                        #tasks管理
+    get  'login',       to: 'user_sessions#new'                      #管理者ログイン画面
+    post 'login',       to: 'user_sessions#create'                   #管理者ログイン処理
+    delete 'logout',    to: 'user_sessions#destroy'                  #管理者ログアウト処理
+    resources :projects, except: %i[new create]                      #projects管理
+    resources :workspaces, except: %i[new create]                    #workspaces管理
+    resources :tasks, except: %i[new create]                         #tasks管理
   end
 
-  get  'login',       to: 'user_sessions#new'                       #ユーザーログイン画面
-  post 'login',       to: 'user_sessions#create'                    #ユーザーログイン処理
-  delete 'logout',    to: 'user_sessions#destroy'                   #ユーザーログアウト処理
-  resources :password_resets, only: %i[new create edit update]      #パスワードリセット
+  get  'login',       to: 'user_sessions#new'                        #ユーザーログイン画面
+  post 'login',       to: 'user_sessions#create'                     #ユーザーログイン処理
+  delete 'logout',    to: 'user_sessions#destroy'                    #ユーザーログアウト処理
+  resources :password_resets, only: %i[new create edit update]       #パスワードリセット
 
-  resources :projects do                                            #projects
-    resources :tasks                                                #tasks
-    resource :preview, only: :show, module: 'projects'              #task設定時のprojectのプレビュー表示
-    resources :favorites, only: %i[create destroy]                  #projectをお気に入りに追加
+  resources :projects do                                             #projects
+    resources :tasks                                                 #tasks
+    resource :preview, only: :show, module: 'projects'               #task設定時のprojectのプレビュー表示
+    resources :favorites, only: %i[create destroy]                   #projectをお気に入りに追加
     collection do
-      get 'search'                                                  #projectsの検索
-      resource :recent, only: :show, module: 'projects'             #projectの新規作成順
-      resource :hot, only: :show, module: 'projects'                #お気に入りの多いproject順
+      get 'search'                                                   #projectsの検索
+      resource :recent, only: :show, module: 'projects'              #projectの新規作成順
+      resource :hot, only: :show, module: 'projects'                 #お気に入りの多いproject順
     end
   end
 
-  resources :workspaces do                                          #workspaces_CRUD
-    resource :profile,       only: %i[show edit update]             #workspacesの中のprofile
-    resource :relationships, only: %i[create destroy]               #好きなwaorkspaceをお気に入りに追加
+  resources :workspaces do                                           #workspaces_CRUD
+    resource :profile,          only: %i[show edit update]           #workspacesの中のprofile
+    resource :relationships,    only: %i[create destroy]             #好きなwaorkspaceをお気に入りに追加
     member do
-      resource :tool_set,    except: :new                           #任意のworkspaceの道具一覧(workspaceのnew時に一緒にnewする)
-      resources :project,    only: :index                           #プロジェクト一覧
-      resources :followers,  only: :index                           #フォロワー一覧
-      resources :followings, only: :index                           #フォロー一覧
-      resources :favorites,  only: :index                           #お気に入り一覧
+      resources :tool_set                                            #任意のworkspaceの道具一覧(workspaceのnew時に一緒にnewする)
+      resources :project,       only: :index                         #プロジェクト一覧
+      resources :followers,     only: :index                         #フォロワー一覧
+      resources :followings,    only: :index                         #フォロー一覧
+      resources :favorites,     only: :index                         #お気に入り一覧
+      resource  :owner_avatars, only: :destroy, module: 'workspaces'
     end
   end
 end
