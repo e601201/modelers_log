ca0084c076355f74dd8caf63f00c5af5e234d86b
e601201 <k.arthur1111@gmail.com>
2022-07-22 14:33:58 +0900

    "[Add]project_app"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_070231) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_070940) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_070231) do
   create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
-    t.string "image"
+    t.string "project_image"
     t.integer "state", default: 0, null: false
     t.bigint "workspace_id", null: false
     t.datetime "created_at", null: false
