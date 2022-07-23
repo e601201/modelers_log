5f7177f1273b238188919bed53311a6519e4e342
e601201 <k.arthur1111@gmail.com>
2022-07-23 20:57:17 +0900

    "[ADD]spec_file_with_task"

---
+++ spec/views/tasks/index.html.tailwindcss_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(
+        title: "Title",
+        body: "MyText",
+        task_image: "Task Image",
+        usage_time: 2,
+        task_state: 3,
+        project: nil
+      ),
+      Task.create!(
+        title: "Title",
+        body: "MyText",
+        task_image: "Task Image",
+        usage_time: 2,
+        task_state: 3,
+        project: nil
+      )
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    assert_select "tr>td", text: "Title".to_s, count: 2
+    assert_select "tr>td", text: "MyText".to_s, count: 2
+    assert_select "tr>td", text: "Task Image".to_s, count: 2
+    assert_select "tr>td", text: 2.to_s, count: 2
+    assert_select "tr>td", text: 3.to_s, count: 2
+    assert_select "tr>td", text: nil.to_s, count: 2
+  end
+end
