96ec650dd176b4e6c7a99a80fa4bd7ee373b461c
e601201 <k.arthur1111@gmail.com>
2022-07-25 14:57:15 +0900

    [fix]プロジェクトへのステータフォームの追加とステータスの変更

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,8 +6,11 @@ class Project < ApplicationRecord
   # has_many :project_tags, dependent: :destroy
   # has_many :notifications, dependent: :destroy
 
-  enum state: { in_progress: 0, published: 5, publish_wait: 10 }
+  enum state: { in_progress: 0, done: 5, published: 10 }
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
   validates :project_image, images: { purge: true, content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 524_288_000 }
+
+  scope :recent_dones, -> { done.order(created_at: :desc) }
+  scope :recent_published, -> { published.order(created_at: :desc) }
 end
