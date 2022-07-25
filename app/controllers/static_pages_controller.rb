6adc0de2f145d2d2a2eb975eb64fc5dccc787611
e601201 <k.arthur1111@gmail.com>
2022-07-25 14:54:35 +0900

    "[refactor]welcomepage"

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: :welcome
   def welcome
-    @projects = Project.all.order(created_at: :desc)
-    @workspaces = Workspace.all.order(created_at: :desc)
+    @projects = Project.includes(:tasks).order(created_at: :desc)
+    @workspaces = Workspace.includes(:projects).order(created_at: :desc)
     # 後でdecoclassにつける page(params[:page]).per(6)
   end
 end
