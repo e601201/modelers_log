e088682d4ed28f63c34cf7c7b75a91ad2a05ef67
e601201 <k.arthur1111@gmail.com>
2022-08-01 15:34:22 +0900

    [feat]カルーセルによるproject,taskの画像の一覧表示実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 255 }
   validates :task_image, images: { purge: true, content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 524_288_000 }
+
+  scope :image_attached_tasks, -> { select{ |a| a.task_image.attached? } }
+  scope :recent_in_progress, -> { in_progress.order(created_at: :desc) }
 end
