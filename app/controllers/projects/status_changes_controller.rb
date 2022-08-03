9c4a79df6bb92c903863d20e243c8f968a7f49fa
e601201 <k.arthur1111@gmail.com>
2022-08-04 00:36:02 +0900

    [fix]add_status_see_other

--- app/controllers/projects/status_changes_controller.rb
+++ app/controllers/projects/status_changes_controller.rb
@@ -2,6 +2,6 @@ class Projects::StatusChangesController < ApplicationController
   def update
     @project = current_user.projects.find(params[:project_id])
     @project.change_state
-    redirect_to project_path(@project), success: t('defaults.message.updated', item: Project.model_name.human)
+    redirect_to project_path(@project), success: t('defaults.message.updated', item: Project.model_name.human), status: :see_other
   end
 end
--- app/controllers/tasks/status_changes_controller.rb
+++ app/controllers/tasks/status_changes_controller.rb
@@ -3,6 +3,6 @@ class Tasks::StatusChangesController < ApplicationController
     @project = Project.find(params[:project_id])
     @task = @project.tasks.find(params[:task_id])
     @task.change_state
-    redirect_to project_path(@project), success: t('defaults.message.updated', item: Task.model_name.human)
+    redirect_to project_path(@project), success: t('defaults.message.updated', item: Task.model_name.human), status: :see_other
   end
 end
