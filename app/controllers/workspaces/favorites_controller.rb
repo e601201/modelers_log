d262485518fb40d46024919a98e6a06ccc50b7f0
e601201 <k.arthur1111@gmail.com>
2022-08-06 15:50:11 +0900

    [fix]ログアウト時の権限の変更

--- app/controllers/workspaces/favorites_controller.rb
+++ app/controllers/workspaces/favorites_controller.rb
@@ -1,4 +1,5 @@
 class Workspaces::FavoritesController < ApplicationController
+  skip_before_action :require_login
   def index
     @workspace = Workspace.find(params[:id])
     @projects = @workspace.favorite_projects
--- app/controllers/workspaces/followings_controller.rb
+++ app/controllers/workspaces/followings_controller.rb
@@ -1,4 +1,5 @@
 class Workspaces::FollowingsController < ApplicationController
+  skip_before_action :require_login
   def index
     @workspace = Workspace.find(params[:id])
     @workspaces = @workspace.followings
