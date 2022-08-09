c82abba460632ea97f7cdd488d1c48960b1758eb
e601201 <k.arthur1111@gmail.com>
2022-08-10 03:39:10 +0900

    [refactor]svgのpathをyamlファイルから参照させる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   def page_title(page_title = '')
     base_title = 'Modelers Log'
 
-    page_title.empty? ? base_title : page_title + " | " + base_title
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
+  def emmbed_svg(object)
+    YAML.load_file('icon_svg.yml')[object]
   end
 end
