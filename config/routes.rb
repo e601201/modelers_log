2ed9f395075810eaf20819c42fbbbee5f6b88cc5
e601201 <k.arthur1111@gmail.com>
2022-08-09 17:56:44 +0900

    [feat]templateのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,33 +12,31 @@ Rails.application.routes.draw do
     resources :tasks, except: %i[new create]                         #tasks管理
   end
 
-  scope module: :top do
-    resources :hot_projects,   only: :index
-    resources :hot_workspaces, only: :index
-  end
-
   get  'login',       to: 'user_sessions#new'                        #ユーザーログイン画面
   post 'login',       to: 'user_sessions#create'                     #ユーザーログイン処理
   delete 'logout',    to: 'user_sessions#destroy'                    #ユーザーログアウト処理
   resources :password_resets, only: %i[new create edit update]       #パスワードリセット
 
+  namespace :template do
+    resource :assembly, only: :create                                #projectのテンプレート作成
+    resource :painting, only: :create                                #同上
+    resource :photography, only: :create                             #同上
+  end
+
   resources :projects do                                             #projects
-    resources :tasks, except: :index do                                 #tasks
-      resource :status_change, only: :update, module: 'tasks'
+    resources :tasks, except: :index do                              #tasks
+      resource :status_change, only: :update, module: 'tasks'        #task_statusの変更
     end
-    resource :status_change, only: :update, module: 'projects'
+    resource :status_change, only: :update, module: 'projects'       #project_statusの変更
     resource :duplications, only: :create, module: 'projects'        #projectを自身のworkspaceに複製する
     resource :preview, only: :show, module: 'projects'               #task設定時のprojectのプレビュー表示
     resources :favorites, only: %i[create destroy]                   #projectをお気に入りに追加
-    collection do
-      resource :recent, only: :show, module: 'projects'              #projectの新規作成順
-    end
   end
 
-  resources :workspaces do                                           #workspaces_CRUD
-    resource :profile,          only: :show, module: 'workspaces'    #workspacesの中のprofile
+  resources :workspaces do                                           #workspaces
+    resource :profile,          only: :show, module: 'workspaces'    #workspaces/profile
     resources :notifications, only: [] do
-      resource :read, only: :create
+      resource :read, only: :create                                  #通知の既読の有無
     end
     resource :relationships,    only: %i[create destroy]             #好きなwaorkspaceをお気に入りに追加
     member do
@@ -47,7 +45,7 @@ Rails.application.routes.draw do
       resources :followings,    only: :index, module: 'workspaces'   #フォロー一覧
       resources :favorites,     only: :index, module: 'workspaces'   #お気に入り一覧
       resources :notifications, only: :index, module: 'workspaces'   #通知一覧
-      resource  :owner_avatars, only: :destroy, module: 'workspaces'
+      resource  :owner_avatars, only: :destroy, module: 'workspaces' #アバター画像の削除
     end
   end
 end
