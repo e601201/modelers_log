e01a41bec918264a080934abcee9ba70b8e41673
e601201 <k.arthur1111@gmail.com>
2022-08-09 17:15:16 +0900

    [fix]taskのcrud操作に関する権限の変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,21 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
-  before_action :set_projecct, only: %i[show new edit create update]
+  before_action :set_projecct, only: %i[show new edit create update destroy]
   skip_before_action :require_login, only: :show
 
   def show; end
 
   def new
+    authorize! @project
     @task = @project.tasks.new
   end
 
-  def edit; end
+  def edit
+    authorize! @project
+  end
 
   def create
+    authorize! @project
     @task = @project.tasks.build(task_params)
 
     if @task.save
@@ -22,6 +26,7 @@ class TasksController < ApplicationController
   end
 
   def update
+    authorize! @project
     if @task.update(task_params)
       redirect_to @project, success: t('defaults.message.updated', item: Task.model_name.human)
     else
@@ -31,6 +36,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    authorize! @project
     @task.destroy!
     redirect_to @task.project, success: t('defaults.message.deleted', item: Workspace.model_name.human), status: :see_other
   end
