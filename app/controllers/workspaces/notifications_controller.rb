9f520f54182116d5bce0d3778db4c4b1b165a5ea
e601201 <k.arthur1111@gmail.com>
2022-08-11 19:11:53 +0900

    [feat]通知のreadカラムの状態変更

--- app/controllers/workspaces/notifications_controller.rb
+++ app/controllers/workspaces/notifications_controller.rb
@@ -1,5 +1,6 @@
 class Workspaces::NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.recent(20)
+    @notifications.map(&:read!)
   end
 end
