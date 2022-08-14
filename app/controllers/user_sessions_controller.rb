9543b7af470ab327136f84c7fa86fb60959eb6ce
e601201 <k.arthur1111@gmail.com>
2022-08-14 18:30:04 +0900

    [feat]guest_loginの導入

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[new create guest_login]
   def new; end
 
   def create
@@ -15,5 +15,20 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     redirect_to root_path, success: t('.success')
+  end
+
+  def guest_login
+    email_account = SecureRandom.uuid
+    @workspace = Workspace.create(
+      owner_name: 'ゲストユーザー',
+      owner_comment: 'ゲストユーザです',
+      email: email_account + '@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      role: :guest
+    )
+    @workspace.update!(owner_name: "ゲストユーザー_#{@workspace.id}")
+    auto_login(@workspace)
+    redirect_back_or_to root_path, success: 'ゲストユーザーとしてログインしました'
   end
 end
