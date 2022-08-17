ced10d979d4102bdfe928011d23fc9802dd1dbae
e601201 <k.arthur1111@gmail.com>
2022-08-17 20:25:43 +0900

    [fix]rubocop

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,13 @@
-class Admin::BaseController < ActionController::Base
+class Admin::BaseController < ApplicationController
   before_action :check_admin
 
   private
 
+  def not_authenticated
+    raise ActionController::RoutingError, 'Not Found'
+  end
+
   def check_admin
-    raise ActionController::RoutingError.new('Not Found') unless current_user&.admin?
+    raise ActionController::RoutingError, 'Not Found' unless current_user&.admin?
   end
 end
