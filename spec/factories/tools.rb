6460a68e0ff888c295e62f0d060733b1bcef53b6
e601201 <k.arthur1111@gmail.com>
2022-09-20 23:45:34 +0900

    [fix]factory/toolのbodyカラムの編集

--- spec/factories/tools.rb
+++ spec/factories/tools.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tool do
     tool_category { 0 }
-    name { "MyString" }
-    text { "Mystring" }
-    workspace { nil }
+    name { "my_tool" }
+    body { "my_tool_text" }
+    association :workspace
   end
 end
